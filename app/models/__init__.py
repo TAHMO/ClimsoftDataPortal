a9f7edb3dd7fe3f4b425158c63df3d71262b5c42
Rick Hagenaars <rick_hagenaars@msn.com>
2021-02-09 18:01:29 +0100

    Portal user and access db models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,7 +8,9 @@ engine = create_engine('mysql+pymysql://root:byMWim84CdUo@172.23.80.1/mariadb_cl
 
 from app.models import station
 from app.models import variable
+from app.models import user
 
+Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 db = scoped_session(DBSession)
 Base.query = db.query_property()
---
+++ app/models/user.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Integer, BigInteger, ForeignKey, String, Column, DateTime, Enum, Float, Text, Boolean
+from sqlalchemy.orm import relationship, backref
+from app.models.base import Base
+from flask_security import UserMixin, RoleMixin
+
+class RolesUsers(Base):
+    __tablename__ = 'portal_roles_users'
+    id = Column(Integer(), primary_key=True)
+    user_id = Column('user_id', Integer(), ForeignKey('portal_user.id'))
+    role_id = Column('role_id', Integer(), ForeignKey('portal_role.id'))
+
+class Role(Base, RoleMixin):
+    __tablename__ = 'portal_role'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(80), unique=True)
+    description = Column(String(255))
+
+class AccessStation(Base):
+    __tablename__ = 'portal_access_station'
+    id = Column(Integer(), primary_key=True)
+    user_id = Column('user_id', Integer(), ForeignKey('portal_user.id'))
+    station_id = Column('station_id', String(255), ForeignKey('station.stationId'))
+
+class AccessVariable(Base):
+    __tablename__ = 'portal_access_variable'
+    id = Column(Integer(), primary_key=True)
+    user_id = Column('user_id', Integer(), ForeignKey('portal_user.id'))
+    variable_id = Column('variable_id', BigInteger(), ForeignKey('obselement.elementId'))
+
+class User(Base, UserMixin):
+    __tablename__ = 'portal_user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), unique=True)
+    username = Column(String(255))
+    password = Column(String(255))
+    last_login_at = Column(DateTime())
+    current_login_at = Column(DateTime())
+    last_login_ip = Column(String(100))
+    current_login_ip = Column(String(100))
+    login_count = Column(Integer)
+    active = Column(Boolean())
+    access_stations_all = Column(Boolean())
+    access_variables_all = Column(Boolean())
+    access_variables_standard = Column(Boolean())
+    confirmed_at = Column(DateTime())
+    roles = relationship('Role', secondary='portal_roles_users',
+                         backref=backref('portal_users', lazy='dynamic'))
+    access_stations_specific = relationship('AccessStation',
+                         backref=backref('portal_users', lazy='dynamic'))
+    access_variable_specific = relationship('AccessVariable',
+                         backref=backref('portal_users', lazy='dynamic'))
