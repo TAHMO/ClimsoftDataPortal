4895b3a827310d369d3fda80975c201b302e5647
Rick Hagenaars <rick_hagenaars@msn.com>
2021-02-15 16:11:40 +0100

    Updated database URI to environment variable

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from app.models.base import Base
 
-# TODO: Remove hardcoded connection URI.
-engine = create_engine('mysql+pymysql://root:byMWim84CdUo@172.23.80.1/mariadb_climsoft_db_v4')
+engine = create_engine('mysql+pymysql://{0}'.format(os.environ.get("DATABASE_URI")))
 
 from app.models import station
 from app.models import variable
