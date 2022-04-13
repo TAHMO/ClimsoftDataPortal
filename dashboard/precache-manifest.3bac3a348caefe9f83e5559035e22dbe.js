27f64ee8c5d71c5124f0be34795435d2eca0ccfc
Rick Hagenaars <rick_hagenaars@msn.com>
2022-04-13 15:48:17 +0200

    New dashboard release for DGM (2022-04-13)

---
+++ dashboard/precache-manifest.3bac3a348caefe9f83e5559035e22dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/portal/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "9d0029aa1b9d6035a90e",
+    "url": "/portal/static/js/runtime~main.9d0029aa.js"
+  },
+  {
+    "revision": "f6902ea1972e51b2fa7f",
+    "url": "/portal/static/js/main.f6902ea1.chunk.js"
+  },
+  {
+    "revision": "8857be92ccf41502f37a",
+    "url": "/portal/static/js/1.8857be92.chunk.js"
+  },
+  {
+    "revision": "f6902ea1972e51b2fa7f",
+    "url": "/portal/static/css/main.c7ae27aa.chunk.css"
+  },
+  {
+    "revision": "8857be92ccf41502f37a",
+    "url": "/portal/static/css/1.dff2f347.chunk.css"
+  },
+  {
+    "revision": "aeabdb79a661759688d6a28af34a370a",
+    "url": "/portal/index.html"
+  }
+];
